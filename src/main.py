400d2e6df5fcab059d6f94153c27ee9e4e68397e
anand-kamble <anandmk837@gmail.com>
2024-05-14 20:54:23 -0400

    feat(filter_configs): added a filter for descriptions of the event
    added an line to export the data to a csv file.
    
    Categorization by ATA in progress

--- src/main.py
+++ src/main.py
@@ -5,15 +5,14 @@ import pandas as pd
 from copa_modules import _types, data_processor
 
 BASE_PATH = "copa/"
-
 config: _types.data_config | None = None
-with open("data_config.json") as f:
+# Path of the config file relative to the run.sh file.
+with open("filter_configs/ata_filter.json") as f:
     config = json.load(f)
-
 # Create a DataProcessor object with the configuration and base path
 my_data_processor = data_processor(
     config,
-    base_path=BASE_PATH,  # Path of the Directory where the dataset is located.
+    base_path=BASE_PATH,  # Path of the Directory where the dataset is located relative to run.sh.
     test_mode=True,  # Test mode which will only load a subset of the data
     test_rows=33000,  # Number of rows to load in test mode
     drop_duplicates=True,  # Drop duplicates from the dataset (Currently set true for Copa dataset)
@@ -22,5 +21,8 @@ my_data_processor = data_processor(
 
 # Below Load function is returning the filtered dataframe.
 my_filtered_data = my_data_processor.load()
-
 print(f"Shape of filtered data: {my_filtered_data.shape}")
+
+event_desc = my_filtered_data["EVENT_SDESC"].value_counts(dropna=False)
+
+event_desc.to_csv("event_desc.csv")
