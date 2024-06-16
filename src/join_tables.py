141d2cef15665938884fc05113b91c98ed1946a6
erlebach <gordon.erlebach@gmail.com>
2024-06-16 16:08:43 -0400

    Automatic sep determination in files
    - updated list of common columns between pairs of files.

--- src/join_tables.py
+++ src/join_tables.py
@@ -2,6 +2,8 @@
 import glob
 import json
 import os
+import re
+from collections import defaultdict
 from functools import reduce
 from pprint import pprint
 
@@ -19,17 +21,36 @@ files_orig = glob.glob("../copa/*.csv")
 files = files_orig + glob.glob("../TABLES_ADD_20240515/*.csv")
 print(f"Found {len(files)} files.")
 
+# %% For each file, identify whether they use "," or ";" as the delimiter
+delimiters = dict()
+for f in files:
+    with open(f, "r", encoding="latin-1") as file:
+        first_line = file.readline()
+        if ";" in first_line:
+            delimiters[f] = ";"
+        else:
+            delimiters[f] = ","
+
+# sve the deliminters dict to the file delimiters.json
+with open("delimiters.json", "w") as f:
+    json.dump(delimiters, f)
+
+pprint(delimiters)
+
 # %% Create a dictionary to store data from each file
 data = dict()
+base_to_full = dict()
 for f in files:
-    print(f"Reading file:{f}")
+    print(f"==> Reading file:{f}")
+    sep = delimiters[f]
     # Read the first 20 lines of each file, skipping bad lines, and store in the dictionary
-    read_df = pd.read_csv(f, nrows=1, on_bad_lines="skip", encoding="latin-1")
+    read_df = pd.read_csv(f, nrows=1, on_bad_lines="skip", encoding="latin-1", sep=sep)
+    print("  - nb columns: ", len(read_df.columns))
     base_filename = os.path.basename(f)
+    base_to_full[base_filename] = f
     data[base_filename] = set(list(read_df.columns))
 
-# pprint(data)
-
+# %%
 comms = dict()
 keys = list(data.keys())
 
@@ -40,6 +61,63 @@ for i, key in enumerate(keys):
 for k, v in comms.items():
     if len(v) > 0:
         print(f"key: {k} ==> Common keys: {v}")
+
+# %% Generate the list of joinable columns across databases into a set
+# and keep unique elements
+
+cols = set()
+for k, v in comms.items():
+    # Skip empty sets
+    if len(v) == 0:
+        continue
+    key1, key2 = k
+    cols = cols.union(data[key1].union(data[key2]))
+
+print(f"Joinable Columns:\n{cols}")
+
+#### END Gordon Mods PROGRAM ####
+
+# %% ## More Gordon Mods
+# Diagnostic checks. For each pair of csv files with common columns, check which columns are unique.
+# Only unique columns can be used for merging/joining the two dataframes.
+# Example: key: ('REQ_PART.csv', 'SD_FAULT.csv') ==> Common keys: {'CREATION_DT', 'REVISION_DT', 'RSTAT_CD', 'ALT_ID'}
+
+try:
+    pd.read_csv("../copa/INV_LOC.csv", encoding="latin-1", nrows=10)
+except Exception as e:
+    print("Exception")
+    print(e)
+# pd.read_csv("../copa/REQ_PART.csv", encoding="latin-1")  # OK
+
+# %%
+
+unique_keys_dict = defaultdict(list)
+for key, cols in comms.items():
+    # key = ("REQ_PART.csv", "SD_FAULT.csv")
+    print("key: ", key)
+    print("Read from files: ", base_to_full[key[0]], base_to_full[key[1]])
+    try:
+        df1 = pd.read_csv(base_to_full[key[0]], encoding="latin-1")
+    except Exception as e:
+        print("Error reading file ", base_to_full[key[0]])
+        continue
+
+    try:
+        df2 = pd.read_csv(base_to_full[key[1]], encoding="latin-1")
+    except Exception as e:
+        print("Error reading file ", base_to_full[key[1]])
+        continue
+
+    for col in cols:
+        nb_non_unique_1 = df1[col].shape[0] - df1[col].nunique()
+        nb_non_unique_2 = df2[col].shape[0] - df2[col].nunique()
+        if nb_non_unique_2 == 0 and nb_non_unique_1 == 0:
+            print("- Unique col: ", col)
+            unique_keys_dict[key].append(col)
+
+    # print(f"Column {col} has {nb_non_unique_1} non-unique values in {key[0]}")
+    # print(f"Column {col} has {nb_non_unique_2} non-unique values in {key[1]}")
+
 # %% Create a dictionary to store common columns between each pair of files
 comms = dict()
 for f in files:
@@ -51,9 +129,8 @@ for f in files:
                 set.intersection, map(set, [data[f].columns, data[ff].columns])
             )
 
+
 # %%
-
-
 if print_common_cols_flag:
 
     class SetEncoder(json.JSONEncoder):
