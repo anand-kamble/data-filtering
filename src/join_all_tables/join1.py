c78b079f5737ed4a84425f70685c4a54482e4be2
erlebach <gordon.erlebach@gmail.com>
2024-06-26 20:23:47 -0400

    Work in progress.

--- src/join_all_tables/join1.py
+++ src/join_all_tables/join1.py
@@ -23,17 +23,17 @@ evt_event_rel = (
 
 # %%
 
-## sched_action = pd.read_parquet(BASE+'sched_action.parquet')
-## evt_inv = pd.read_parquet(BASE+'evt_inv.parquet')
+sched_action = pd.read_parquet(BASE + "sched_action.parquet")
+evt_inv = pd.read_parquet(BASE + "evt_inv.parquet")
 ## # inv_inv = pd.read_parquet(BASE+'inv_inv.parquet')  # does not exist
-## inv_ac_reg = pd.read_parquet(BASE+'inv_ac_reg.parquet')
-## evt_loc = pd.read_parquet(BASE+'evt_loc.parquet')
-## inv_loc = pd.read_parquet(BASE+'inv_loc.parquet')
-## eqp_assmbl = pd.read_parquet(BASE+'eqp_assmbl.parquet')
-## eqp_assmbl_bom = pd.read_parquet(BASE+'eqp_assmbl_bom.parquet')
-## ref_event_status = pd.read_parquet(BASE+'ref_event_status.parquet')
-## fl_leg = pd.read_parquet(BASE+'fl_leg.parquet')
-## fl_leg_disrupt = pd.read_parquet(BASE+'fl_leg_disrupt.parquet')
+inv_ac_reg = pd.read_parquet(BASE + "inv_ac_reg.parquet")
+evt_loc = pd.read_parquet(BASE + "evt_loc.parquet")
+inv_loc = pd.read_parquet(BASE + "inv_loc.parquet")
+eqp_assmbl = pd.read_parquet(BASE + "eqp_assmbl.parquet")
+eqp_assmbl_bom = pd.read_parquet(BASE + "eqp_assmbl_bom.parquet")
+ref_event_status = pd.read_parquet(BASE + "ref_event_status.parquet")
+fl_leg = pd.read_parquet(BASE + "fl_leg.parquet")
+fl_leg_disrupt = pd.read_parquet(BASE + "fl_leg_disrupt.parquet")
 
 # Perform the joins
 df1 = sd_fault.merge(
@@ -78,8 +78,6 @@ print("df2 columns:", sorted(list(df2.columns)))
 #     how="inner",
 #     suffixes=("", "_right"),
 # )
-# %%
-3
 
 # %%
 cols_with_x = df2.columns[df2.columns.str.endswith("_x")]
@@ -107,8 +105,6 @@ for col in cols:
     frac_same = (col1_series == col2_series).sum() / df_interleaved.shape[0]
     print(f"{col=}, {frac_unique_1=}, {frac_unique_2=}, {frac_same=}")
 
-
-quit()
 # %%
 # Compare each pair of columns and list the number of mismatched elements as a fraction of the total number of rows. Use df_interleaved as a starting point.
 for col_x, col_y in zip(cols_with_x, cols_with_y):
@@ -143,8 +139,16 @@ for col_x, col_y in zip(cols_with_x, cols_with_y):
 #     FAULT_EVENT.EVENT_ID    = EVT_EVENT_REL.REL_EVENT_ID AND
 #     REL_TYPE_CD = 'DISCF'
 
+# %%
 # What is DISCF?  <<<<<<<
-
+# MERGE 4
+"""
+     LEFT JOIN EVT_EVENT_REL ON
+        FAULT_EVENT.EVENT_DB_ID = EVT_EVENT_REL.REL_EVENT_DB_ID AND
+        FAULT_EVENT.EVENT_ID    = EVT_EVENT_REL.REL_EVENT_ID AND
+        REL_TYPE_CD = 'DISCF'
+     -- #### This join does nothing.
+"""
 df4 = df3.merge(
     evt_event_rel[evt_event_rel["REL_TYPE_CD"] == "DISCF"],
     left_on=["EVENT_DB_ID", "EVENT_ID"],
@@ -152,6 +156,363 @@ df4 = df3.merge(
     how="left",
     suffixes=("", "_EVT_REL"),
 )
+
+# Which are the common columns?
+
+# %%
+# MERGE 5
+"""
+      LEFT JOIN SCHED_STASK FOUND_ON_TASK ON
+        EVT_EVENT_REL.EVENT_DB_ID = FOUND_ON_TASK.SCHED_DB_ID AND
+        EVT_EVENT_REL.EVENT_ID    = FOUND_ON_TASK.SCHED_ID
+"""
+df5 = df4.merge(
+    sched_stask,
+    left_on=["EVENT_DB_ID", "EVENT_ID"],
+    right_on=["SCHED_DB_ID", "SCHED_ID"],
+    how="left",
+    suffixes=("", "_SCHED_STASK"),
+)
+
+print("Merge 5 done")
+
+# %%
+# Merge 6
+"""
+      LEFT JOIN EVT_EVENT FOUND_ON_EVENT ON
+        EVT_EVENT_REL.EVENT_DB_ID = FOUND_ON_EVENT.EVENT_DB_ID AND
+        EVT_EVENT_REL.EVENT_ID    = FOUND_ON_EVENT.EVENT_ID
+"""
+
+found_on_event = df6 = df5.merge(
+    evt_event,
+    left_on=["EVENT_DB_ID", "EVENT_ID"],
+    right_on=["EVENT_DB_ID", "EVENT_ID"],
+    how="left",
+    suffixes=("", "_event_evt3"),  # 3rd time event-evt used
+)
+
+print(f"Merge 6 done, {df6.shape=}")
+
+# %%
+# MERGE 7
+"""
+      LEFT JOIN EVT_EVENT WORK_PKG_EVENT ON
+        WORK_PKG_EVENT.EVENT_DB_ID = TASK_EVENT.H_EVENT_DB_ID AND
+        WORK_PKG_EVENT.EVENT_ID    = TASK_EVENT.H_EVENT_ID
+"""
+
+work_pkg_event = evt_event
+df7 = df6.merge(
+    work_pkg_event,
+    left_on=["H_EVENT_DB_ID", "H_EVENT_ID"],
+    right_on=["EVENT_DB_ID", "EVENT_ID"],
+    how="left",
+    suffixes=("", "_work_pkg_event_7"),  # 3rd time event-evt used
+)
+print(f"Merge 7 done, {df7.shape=}")
+
+
+# %%
+# MERGE 8
+"""
+    LEFT JOIN SCHED_STASK WORK_PKG_SCHED_STASK ON
+      WORK_PKG_SCHED_STASK.SCHED_DB_ID = WORK_PKG_EVENT.EVENT_DB_ID AND
+      WORK_PKG_SCHED_STASK.SCHED_ID    = WORK_PKG_EVENT.EVENT_ID AND
+      WORK_PKG_SCHED_STASK.TASK_CLASS_CD in ('CHECK','RD')
+"""
+work_pkg_sched_stask = sched_stask
+df8 = df7.merge(
+    work_pkg_sched_stask[work_pkg_sched_stask["TASK_CLASS_CD"].isin(["CHECK", "RD"])],
+    left_on=["EVENT_DB_ID", "EVENT_ID"],
+    right_on=["SCHED_DB_ID", "SCHED_ID"],
+    how="left",
+    suffixes=("", "_evt_event_work_8"),
+)
+print(f"Merge 8 done, {df8.shape=}")
+
+# %%
+# MERGE 9
+"""
+     LEFT OUTER JOIN SCHED_ACTION ON
+        CORR_TASK_SS.SCHED_DB_ID = SCHED_ACTION.SCHED_DB_ID AND
+        CORR_TASK_SS.SCHED_ID =  SCHED_ACTION.SCHED_ID
+"""
+
+df9 = df8.merge(
+    sched_action,
+    left_on=["SCHED_DB_ID", "SCHED_DB_ID"],
+    right_on=["SCHED_DB_ID", "SCHED_ID"],
+    how="left",
+    suffixes=("", "_sched_action_9"),
+)
+
+print(f"Merge 9 done, {df9.shape=}")
+
+# %%
+# MERGE 10
+"""
+      LEFT JOIN EVT_INV ON
+        EVT_INV.EVENT_DB_ID = FAULT_EVENT.EVENT_DB_ID AND
+        EVT_INV.EVENT_ID    = FAULT_EVENT.EVENT_ID
+"""
+df10 = df9.merge(
+    evt_inv,
+    left_on=["EVENT_DB_ID", "EVENT_ID"],
+    right_on=["EVENT_DB_ID", "EVENT_ID"],
+    how="left",
+    suffixes=("", "_evt_inv_10"),
+)
+
+print(f"Merge 10 done, {df10.shape=}")
+# %%
+# MERGE 11
+"""
+      LEFT JOIN INV_INV ON
+        INV_INV.INV_NO_DB_ID = EVT_INV.H_INV_NO_DB_ID AND
+        INV_INV.INV_NO_ID    = EVT_INV.H_INV_NO_ID
+"""
+df11 = df10
+
+# Missing INV_INV table
+
+print(f"Merge 11 done, {df11.shape=}")
+# %%
+# MERGE 12
+"""
+      LEFT JOIN INV_AC_REG ON
+        INV_INV.INV_NO_DB_ID = INV_AC_REG.INV_NO_DB_ID AND
+        INV_INV.INV_NO_ID = INV_AC_REG.INV_NO_ID
+"""
+df12 = df11.merge(
+    inv_ac_reg,
+    left_on=["H_INV_NO_DB_ID", "H_INV_NO_ID"],
+    right_on=["INV_NO_DB_ID", "INV_NO_ID"],
+    how="left",
+    suffixes=("", "_inv_ac_reg_12"),
+)
+print(f"Merge 12 done, {df12.shape=}")
+
+# %%
+# MERGE 13
+"""
+      LEFT JOIN INV_INV ACFT_II_NEW ON
+        ACFT_II_NEW.INV_NO_DB_ID = CORR_TASK_SS.MAIN_INV_NO_DB_ID AND
+        ACFT_II_NEW.INV_NO_ID    = CORR_TASK_SS.MAIN_INV_NO_ID
+"""
+df13 = df12
+# MISSING INV_INV
+print(f"Merge 13 done, {df13.shape=}")
+
+# %%
+# MERGE 14
+"""
+      LEFT JOIN INV_AC_REG ACFT_IAR_NEW ON
+        ACFT_IAR_NEW.INV_NO_DB_ID = ACFT_II_NEW.H_INV_NO_DB_ID AND
+        ACFT_IAR_NEW.INV_NO_ID = ACFT_II_NEW.H_INV_NO_ID
+"""
+# Print the column names that contain with "INV"
+# print([col for col in df13.columns if "INV" in col])
+# print()
+# print(f"{inv_ac_reg.columns=}")
+
+# inv_ac_reg has columns INV_NO_DB_ID, etc.
+# df13 has columns H_INV_NO_DB_ID, etc.
+
+acft_iar_new = inv_ac_reg
+df14 = df13.merge(
+    acft_iar_new,
+    # These columns are in df13 (should be in inv_ac_reg)
+    left_on=["MAIN_INV_NO_DB_ID", "MAIN_INV_NO_ID"],
+    right_on=["INV_NO_DB_ID", "INV_NO_ID"],
+    how="left",
+    suffixes=("", "_acft_iar_new_14"),
+)
+
+print(f"Merge 14 done, {df14.shape=}")
+# %%
+# MERGE 15
+"""
+      LEFT JOIN EVT_LOC ON
+        EVT_LOC.EVENT_DB_ID = WORK_PKG_EVENT.EVENT_DB_ID AND
+        EVT_LOC.EVENT_ID    = WORK_PKG_EVENT.EVENT_ID
+"""
+
+df15 = df14.merge(
+    evt_loc,
+    # These columns are in df13 (should be in inv_ac_reg)
+    left_on=["EVENT_DB_ID", "EVENT_ID"],
+    right_on=["EVENT_DB_ID", "EVENT_ID"],
+    how="left",
+    suffixes=("", "_evt_loc_14"),
+)
+
+print(f"Merge 15 done, {df15.shape=}")
+
+# %%
+# MERGE 16
+"""
+     LEFT JOIN INV_LOC ON
+        EVT_LOC.LOC_DB_ID = INV_LOC.LOC_DB_ID AND
+        EVT_LOC.LOC_ID    = INV_LOC.LOC_ID
+"""
+
+df16 = df15.merge(
+    inv_loc,
+    # These columns are in df13 (should be in inv_ac_reg)
+    left_on=["LOC_DB_ID", "LOC_ID"],
+    right_on=["LOC_DB_ID", "LOC_ID"],
+    how="left",
+    suffixes=("", "_inv_loc_16"),
+)
+
+
+print(f"Merge 16 done, {df16.shape=}")
+
+# %%
+# Release memory
+del df1, df2, df3, df4, df5, df6, df7, df8, df9, df10, df11, df12, df13, df14, df15
+
+
+# %%
+# MERGE 17
+"""
+      INNER JOIN EQP_ASSMBL ON
+        EQP_ASSMBL.ASSMBL_DB_ID = EVT_INV.ASSMBL_DB_ID AND
+        EQP_ASSMBL.ASSMBL_CD    = EVT_INV.ASSMBL_CD
+"""
+df17 = df16.merge(
+    eqp_assmbl,
+    # These columns are in df13 (should be in inv_ac_reg)
+    left_on=["ASSMBL_DB_ID", "ASSMBL_CD"],
+    right_on=["ASSMBL_DB_ID", "ASSMBL_CD"],
+    how="left",
+    suffixes=("", "_eqp_assmbl_17"),
+)
+
+print(f"Merge 17 done, {df17.shape=}")
+# %%
+# MERGE 18
+"""
+      INNER JOIN EQP_ASSMBL_BOM ON
+        EQP_ASSMBL_BOM.ASSMBL_DB_ID  = EVT_INV.ASSMBL_DB_ID AND
+        EQP_ASSMBL_BOM.ASSMBL_CD     = EVT_INV.ASSMBL_CD AND
+        EQP_ASSMBL_BOM.ASSMBL_BOM_ID = EVT_INV.ASSMBL_BOM_ID
+"""
+df18 = df17.merge(
+    eqp_assmbl_bom,
+    # These columns are in df13 (should be in inv_ac_reg)
+    left_on=["ASSMBL_DB_ID", "ASSMBL_CD"],
+    right_on=["ASSMBL_DB_ID", "ASSMBL_CD"],
+    how="left",
+    suffixes=("", "_eqp_assmbl_bom_18"),
+)
+print(f"Merge 18 done, {df18.shape=}")
+
+# %%
+# MERGE 19
+"""
+      INNER JOIN REF_EVENT_STATUS FAULT_STATUS_CODE ON
+        FAULT_EVENT.EVENT_STATUS_DB_ID = FAULT_STATUS_CODE.EVENT_STATUS_DB_ID AND
+        FAULT_EVENT.EVENT_STATUS_CD = FAULT_STATUS_CODE.EVENT_STATUS_CD
+"""
+fault_status_code = ref_event_status
+df19 = df18.merge(
+    fault_status_code,
+    # These columns are in df13 (should be in inv_ac_reg)
+    left_on=["EVENT_STATUS_DB_ID", "EVENT_STATUS_CD"],
+    right_on=["EVENT_STATUS_DB_ID", "EVENT_STATUS_CD"],
+    how="left",
+    suffixes=("", "fault_status_code_19"),
+)
+print(f"Merge 19 done, {df19.shape=}")
+
+# %%
+# MERGE 20
+"""
+      INNER JOIN REF_EVENT_STATUS TASK_STATUS_CODE ON
+        TASK_EVENT.EVENT_STATUS_DB_ID = TASK_STATUS_CODE.EVENT_STATUS_DB_ID AND
+        TASK_EVENT.EVENT_STATUS_CD = TASK_STATUS_CODE.EVENT_STATUS_CD
+"""
+task_status_code = ref_event_status
+df20 = df19.merge(
+    task_status_code,
+    # These columns are in df13 (should be in inv_ac_reg)
+    left_on=["EVENT_STATUS_DB_ID", "EVENT_STATUS_CD"],
+    right_on=["EVENT_STATUS_DB_ID", "EVENT_STATUS_CD"],
+    how="left",
+    suffixes=("", "task_status_code_20"),
+)
+print(f"Merge 20 done, {df20.shape=}")
+
+# %%
+# MERGE 21
+"""
+      LEFT OUTER JOIN FL_LEG FLIGHT ON
+        SD_FAULT.LEG_ID = FLIGHT.LEG_ID
+"""
+flight = fl_leg
+df21 = df20.merge(
+    flight,
+    # These columns are in df13 (should be in inv_ac_reg)
+    left_on="LEG_ID",
+    right_on="LEG_ID",
+    how="left",
+    suffixes=("", "flight_21"),
+)
+print(f"Merge 21 done, {df21.shape=}")
+
+# %%
+# MERGE 22
+"""
+      LEFT OUTER JOIN INV_LOC DEP ON
+        FLIGHT.DEPARTURE_LOC_DB_ID = DEP.LOC_DB_ID AND
+        FLIGHT.DEPARTURE_LOC_ID = DEP.LOC_ID
+"""
+dep = inv_loc
+df22 = df21.merge(
+    dep,
+    # These columns are in df13 (should be in inv_ac_reg)
+    left_on=["LOC_DB_ID", "LOC_ID"],
+    right_on=["LOC_DB_ID", "LOC_ID"],
+    how="left",
+    suffixes=("", "dep_22"),
+)
+print(f"Merge 22 done, {df22.shape=}")
+
+# %%
+# MERGE 23
+"""
+      LEFT OUTER JOIN INV_LOC ARR ON
+        FLIGHT.ARRIVAL_LOC_DB_ID = ARR.LOC_DB_ID AND
+        FLIGHT.ARRIVAL_LOC_ID = ARR.LOC_ID
+"""
+arr = inv_loc
+df23 = df22.merge(
+    arr,
+    left_on=["LOC_DB_ID", "LOC_ID"],
+    right_on=["LOC_DB_ID", "LOC_ID"],
+    how="left",
+    suffixes=("", "task_status_code_23"),
+)
+print(f"Merge 23 done, {df23.shape=}")
+# %%
+# MERGE 24
+"""
+      LEFT JOIN FL_LEG_DISRUPT ON
+        FL_LEG_DISRUPT.SCHED_DB_ID = CORR_TASK_SS.SCHED_DB_ID AND
+        FL_LEG_DISRUPT.SCHED_ID = CORR_TASK_SS.SCHED_ID
+"""
+df24 = df23.merge(
+    fl_leg_disrupt,
+    # These columns are in df13 (should be in inv_ac_reg)
+    left_on=["SCHED_DB_ID", "SCHED_ID"],
+    right_on=["SCHED_DB_ID", "SCHED_ID"],
+    how="left",
+    suffixes=("", "fl_leg_disrupt_24"),
+)
+print(f"Merge 24 done, {df24.shape=}")
+quit()
 
 print(f"{df3.shape=}, {evt_event_rel.shape=}, {df4.shape=}")
 print(f"cols(df4): {sorted(list(df4.columns))}")
